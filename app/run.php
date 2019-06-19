c2ca5faf75a2491bc8e3cd8368361ae8ccb07097
lishengwei <lishengwei010@163.com>
2019-06-19 21:05:32 +0800

    修复

--- app/run.php
+++ app/run.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 global $APP_CONFIGS, $exist;
 $APP_CONFIGS = [
     'dir'       => './files',
@@ -29,7 +29,7 @@ $APP_CONFIGS = [
             'device_type' => 'SALSA_DRV_V10.S19', 'row_key' => 'B20000A2', 'qty' => 72,
         ],
         'FS8500_HTOL_R5P1.S19' => [
-            'device_type' => 'FS8500_HTOL_R5P1.S19', 'row_key' => 'B20000A2', 'qty' => 40,
+            'device_type' => 'FS8500_HTOL_R5P1.S19', 'row_key' => '39800000', 'qty' => 40,
         ]
     ],
 ];
@@ -40,9 +40,9 @@ if (!is_dir($APP_CONFIGS['dir'])) {
 
 $dirHandle  = dir($APP_CONFIGS['dir']);
 $handle     = fopen($APP_CONFIGS['dir'] . '/result.csv', 'w+');
-$count      = 0;
+$count      = $count1 = 0;
 $header     = [
-    'Lot Number', 'BIB Number', 'Pass Qty'
+    'Lot Number', 'BIB Number', 'Pass Qty1', 'Pass Qty2'
 ];
 for ($si = 1;$si < 73; $si ++) {
     $header[] = "Socket". str_pad($si, 2, '0',STR_PAD_LEFT);
@@ -61,24 +61,40 @@ while ($file = $dirHandle->read()) {
     if (!isset($result['number_infos']) || empty($result['number_infos'])) {
         continue;
     }
+
     foreach ($result['number_infos'] as $numberInfo) {
         if (!isset($numberInfo['result']) || empty($numberInfo['result'])) {
             continue;
         }
-
+        $repeatNum = 0;
+        $numbers = [];
+        //去重复
+        foreach ($numberInfo['result'] as $v) {
+            if ($v != "F" && isset($exist[$v])) {
+                $numbers[] = "C";
+                $repeatNum++;
+            } else {
+                $numbers[] = "{$v}";
+                $exist[$v] = $v;
+            }
+        }
+echo $numberInfo['all_num'] . " " . $numberInfo['error_num'] . " " . $repeatNum . "\n";
         $lineArray = [
-            0 => $result['file_name_key'],
-            1 => "\"{$numberInfo['number']}\"",
-            2 => $numberInfo['effect_num'],
+            $result['file_name_key'],
+            "\"{$numberInfo['number']}\"",
+            $numberInfo['all_num'] - $numberInfo['error_num'],
+            $numberInfo['all_num'] - $numberInfo['error_num'] - $repeatNum,
         ];
-        foreach ($numberInfo['result'] as $key => $number) {
+
+        foreach ($numbers as $number) {
             $lineArray[] = "\"{$number}\"";
         }
         fputcsv($handle, $lineArray);
-        $count = $count + $lineArray[2];
+        $count = $count + $lineArray[3];
+        $count1 = $count1 + $lineArray[2];
     }
 }
-fputcsv($handle, ['', '', $count]);
+fputcsv($handle, ['', '', $count1, $count]);
 fclose($handle);
 echo "执行成功";
 
@@ -119,13 +135,12 @@ function getNumberInfos($fileName)
 
 function getResult($params)
 {
-    global $APP_CONFIGS, $exist;
+    global $APP_CONFIGS;
     $fileName   = $APP_CONFIGS['dir'] . '/' . $params['file_name_key'] . '_DriverMonitor.log';
 
     if (!file_exists($fileName)) {
         return [];
     }
-    $localExist = [];
     $deviceType = '';
     $secondFileHandler = fopen($fileName, 'r');
     while (($secondFileLineContent = fgets($secondFileHandler)) !== false) {
@@ -148,53 +163,49 @@ function getResult($params)
     $config = $APP_CONFIGS['product_configs'][$deviceType];
     while (($secondFileLineContent = fgets($secondFileHandler)) !== false) {
         $secondFileLineContent = trim($secondFileLineContent);
-        $repeatNum = $errorNum = $effectNum = 0;
+        $errorNum = $effectNum = 0;
         if (empty($secondFileLineContent)) {
             continue;
         }
-        $keyPos = strpos($secondFileLineContent, $config['row_key']);
-        if ($keyPos !== false) {
-            $rowArray   = explode(',', $secondFileLineContent);
-            $didNum     = $rowArray[3];
-            if (!isset($params['number_infos'][$didNum])) {
-                continue;
-            }
-            $qty = $config['qty'];
-            if (is_array($qty)) {
-                foreach ($qty as $didKey => $qtyNum) {
-                    if (strpos($didNum, $didKey) !== false) {
-                        $qty = $qtyNum;
-                        break;
-                    }
-                }
-            }
-
-
-            for ($i = 1; $i <= $qty; $i++) {
-                if (in_array($i, $params['number_infos'][$didNum]['keys'])) {
-                    $id = "F";
-                    $repeatNum ++;
-                } else {
-                    $k          = $i + 5;
-                    $id         = $rowArray[$k];
-                    if (empty($localExist[$id])) {
-                        $localExist[$id] = $id;
-                        if (empty($exist[$id])) {
-                            $exist[$id] = 1;
-                        } else {
-                            $exist[$id]++;
-                            $id = "C"; //重复的
-                            $params['number_infos'][$didNum]['keys'][] = $i;
-                            $repeatNum++;
-                        }
-                    }
-                }
-                $params['number_infos'][$didNum]['result'][$i] = $id;
-            }
-            $params['number_infos'][$didNum]['repeat_num']  = $repeatNum;
-            $params['number_infos'][$didNum]['error_num']   = $errorNum;
-            $params['number_infos'][$didNum]['effect_num']  = count($params['number_infos'][$didNum]['result']) -  $repeatNum - $errorNum;
+        $rowArray   = explode(',', $secondFileLineContent);
+        $keyPos     = false;
+        $rowArrayCount = 0;
+        is_array($rowArray) && $rowArrayCount = count($rowArray);
+        $rowArrayKeyNum = $rowArrayCount - 2;
+        if ($rowArrayKeyNum > 0 && $rowArray[$rowArrayKeyNum] == $config['row_key']) {
+            $keyPos = true;
         }
+        if ($keyPos === false) {
+            continue;
+        }
+
+        $didNum     = $rowArray[3];
+        if (!isset($params['number_infos'][$didNum])) {
+            continue;
+        }
+        $qty = $config['qty'];
+        if (is_array($qty)) {
+            foreach ($qty as $didKey => $qtyNum) {
+                if (strpos($didNum, $didKey) !== false) {
+                    $qty = $qtyNum;
+                    break;
+                }
+            }
+        }
+
+        for ($i = 1; $i <= $qty; $i++) {
+            if (in_array($i, $params['number_infos'][$didNum]['keys'])) {
+                $id = "F";
+                $errorNum ++;
+            } else {
+                $k          = $i + 5;
+                $id         = (string)$rowArray[$k];
+            }
+            $params['number_infos'][$didNum]['result'][$i] = $id;
+        }
+
+        $params['number_infos'][$didNum]['error_num']   = $errorNum;
+        $params['number_infos'][$didNum]['all_num']     = count($params['number_infos'][$didNum]['result']);
     }
 
     $params['device_type'] = $deviceType;
